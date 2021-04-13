fb32f7109ab4d9a4396a7e9c7cde7cdb36ff30b9
Gustavo Sandoval <jgusta.s25@gmail.com>
2021-04-13 16:56:20 -0300

    SPA Control

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const { dbConnection } = require('./database/config');
 
@@ -20,23 +21,24 @@ dbConnection();
 app.use(express.static('public'));
 
 //Rutas
-//Ruta de Usuarios
+
 app.use('/api/usuarios', require('./routes/usuarios'));
 
-//Ruta de hospitales
 app.use('/api/hospitales', require('./routes/hospitales'));
 
-//Ruta de médicos
 app.use('/api/medicos', require('./routes/medicos'));
 
-//Ruta de Login
 app.use('/api/login', require('./routes/auth'));
 
-//Ruta de busqueda
 app.use('/api/todo', require('./routes/busquedas'));
 
-//Ruta de Upload
 app.use('/api/upload', require('./routes/uploads'));
+
+//Ultimo
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
